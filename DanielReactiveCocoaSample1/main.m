9d187db0141bf212d43817cc6fba8cd1e7c1c9d8
Daniel Lam <im@daniellam.name>
2014-04-03 18:37:41 +1100

    Initial Commit

---
+++ DanielReactiveCocoaSample1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DanielReactiveCocoaSample1
+//
+//  Created by Daniel Lam on 3/04/2014.
+//  Copyright (c) 2014 Daniel Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ DanielReactiveCocoaSample1Tests/DanielReactiveCocoaSample1Tests.m
@@ -0,0 +1,34 @@
+//
+//  DanielReactiveCocoaSample1Tests.m
+//  DanielReactiveCocoaSample1Tests
+//
+//  Created by Daniel Lam on 3/04/2014.
+//  Copyright (c) 2014 Daniel Lam. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DanielReactiveCocoaSample1Tests : XCTestCase
+
+@end
+
+@implementation DanielReactiveCocoaSample1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
